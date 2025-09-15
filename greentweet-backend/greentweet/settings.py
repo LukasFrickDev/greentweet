0b07a581c116c8fdf26846bcce8a9fbd79fca195
Lukas Frick <contatolukasfrick@gmail.com>
2025-09-15 16:49:03 -0300

    Profiles + autocreate

--- greentweet-backend/greentweet/settings.py
+++ greentweet-backend/greentweet/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'corsheaders',
     'users',
-
+    'profiles',
 ]
 
 MIDDLEWARE = [
---
+++ greentweet-backend/profiles/models/profile.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Perfil de {self.user.username}"
+
